f86bba3287ff9a6b0cdf09a5e609c593cbaa4def
adireddy <adireddy.gr@gmail.com>
2016-11-16 21:19:30 +0530

    added DemoJava.java file

---
+++ DemoJava.java
@@ -0,0 +1,18 @@
+
+
+public class DemoJava
+{
+
+public static void main(String[] args)
+{
+System.out.println("Demo Program in Java1");
+for(int i=0;i<=10; i++)
+{
+
+System.out.println("I am number:"+i);
+}
+
+}
+
+
+}
